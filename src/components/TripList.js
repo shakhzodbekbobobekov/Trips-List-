4d0e72fc79ab9996eb9c97bc330f073eaa8fdd6b
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-04-24 16:33:49 +0500

    add trips buttons

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,21 +1,43 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import './TripList.css'
 
 function TripList() {
     const [trips, setTrips] = useState([])
-        // fetch('http://localhost:3000/trips')
-        // .then((res)=> res.json())
-        // .then((data)=> setTrips(data))
-        // console.log(trips);
+    const [ url, setUrl ] = useState('http://localhost:3000/trips')   
+
+    // useCallback(0
     
     useEffect(() => {
-         fetch('http://localhost:3000/trips')
+         fetch(url)
         .then((res)=> res.json())
             .then((data) => setTrips(data))
-        },[])
+        },[url])
+        
         console.log(trips);
-
   return (
-    <div>TripList</div>
+      <div className='trip-list'>
+          <h2>TripsList</h2>
+          
+          <ul>
+              {
+                  trips.map((trip) => {
+                      return (
+                          <div>
+                              <li>
+                                  <h2>{ trip.title }</h2>
+                                  <p>{ trip.price }</p>
+                              </li>
+                          </div>
+                      )
+                  })
+              }
+              <div className="filters">
+                <button onClick={() => {setUrl('http://localhost:3000/trips?loc=europe')}} >europe trips</button>
+                <button onClick={() => {setUrl('http://localhost:3000/trips?loc=america')}} >america trips</button>
+                <button onClick={() => {setUrl('http://localhost:3000/trips')}} >All trips</button>
+             </div>
+          </ul>
+    </div>
   )
 }
 
